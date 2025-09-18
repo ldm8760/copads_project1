6bc92abce0d8174fab3d04520a80d139b976370b
ldm8760 <ldm8760@rit.edu>
2025-09-18 18:15:59 -0400

    in-class changes and recommendations from ige

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Reflection;
-
-class CLI_Handler
+﻿class CLI_Handler
 {
     // public string Mode { get; set; }
     // public string Path { get; set; }
@@ -14,7 +12,7 @@ class CLI_Handler
 
     private void handleCLI(string mode, string path)
     {
-        FileHandler fileHandler = new FileHandler(path);
+        // FileHandler fileHandler = new FileHandler(path);
         if (mode == "-s")
         {
             SequentialMode seq = new SequentialMode(path);
@@ -68,11 +66,46 @@ class ParallelMode
 class FileHandler
 {
     public HashSet<string> files = new HashSet<string>();
+    public HashSet<string> directories = new HashSet<string>();
+    public string Path { get; set; }
+    private int folderCount = 0;
+    private int fileCount = 0;
+    private int byteCount = 0;
+
     // this must be recursive
 
     public FileHandler(string path)
     {
-        
+        Path = path;
+    }
+
+    public void findAllPaths(string path)
+    {
+        // base case
+        if (File.Exists(path))
+        {
+            files.Add(path);
+            fileCount += 1;
+        }
+        else if (Directory.Exists(path))
+        {
+            // Parallel.foreach for parallel
+            foreach (string d in Directory.GetDirectories(path))
+            {
+                directories.Add(d);
+                folderCount += 1;
+                findAllPaths(d);
+            }
+            foreach (string f in Directory.GetFiles(path))
+            {
+                files.Add(f);
+                fileCount += 1;
+            }
+        }
+        Console.WriteLine(files);
+        Console.WriteLine(directories);
+        Console.WriteLine(fileCount);
+        Console.WriteLine(folderCount);
     }
 }
 
@@ -80,12 +113,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Test");
-        string[] dir = Directory.GetDirectories("C:\\Users\\extra");
-        // if (Directory.Exists(directoryPath))
-        foreach (string d in dir)
-        {
-            Console.WriteLine($"{d}");
-        }
+        string main = "C:\\Users\\extra";
+        FileHandler fileHandler = new FileHandler(main);
+        fileHandler.findAllPaths(main);
     }
 }
